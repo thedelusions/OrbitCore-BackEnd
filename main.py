b5a14e81a62029dd65653c4ae8240af4f060bc2c
BIOS-SCRIPT <164663501+BIOS-SCRIPT@users.noreply.github.com>
2025-12-16 22:26:24 +0300

    UPDATE: main.py for the Front-End

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers.users import router as UserRouter
 from controllers.projects import router as ProjectRouter
+from database import engine
+from models.base import Base
+
+
+# Create all tables
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5173", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(UserRouter, prefix="/api")
 app.include_router(ProjectRouter, prefix="/api")
 
 @app.get('/')
 def home():
-    return 'Hello World!'
+    return {'message': 'Hello World!'}
