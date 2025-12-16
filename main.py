662ac727d46b73911cb1d226aa108f1d31eeb019
ahq_292 <ali.quraishy2014@email.com>
2025-12-16 23:43:17 +0300

    checking swagger

--- main.py
+++ main.py
@@ -1,31 +1,14 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from controllers.users import router as UserRouter
 from controllers.projects import router as ProjectRouter
-from database import engine
-from models.base import Base
-
-
-# Create all tables
-Base.metadata.create_all(bind=engine)
+from controllers.team import router as TeamRouter
 
 app = FastAPI()
 
-origins = [
-    "http://127.0.0.1:5173", 
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
 app.include_router(UserRouter, prefix="/api")
 app.include_router(ProjectRouter, prefix="/api")
+app.include_router(TeamRouter, prefix="/api")
 
 @app.get('/')
 def home():
-    return {'message': 'Hello World!'}
+    return 'Hello World!'
